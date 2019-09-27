ea16363498a55a02a1ee65f04d4414674421661c
1337eeab <andrewtb97@hotmail.com>
2019-09-27 15:06:47 -0700

    Convex Hull
    Bugfixes

--- Lab1/images/contours.py
+++ Lab1/images/contours.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 
 def contour(image = 'something', percent_arclength = 0.1):
 	img = cv2.imread(image, 0)
+	original_image = cv2.imread(image, 0)
 	ret, thresh = cv2.threshold(img, 127, 255, 0)
 	contours, hierarchy = cv2.findContours(thresh, 1, 2)
 
@@ -14,7 +15,7 @@ def contour(image = 'something', percent_arclength = 0.1):
 
 	#You can then calculate centroids from the following lines:
 	cx = int(M['m10']/M['m00'])
-	cy = int(M['01']/M['m00'])
+	cy = int(M['m01']/M['m00'])
 
 	#Contour areas, perimeter, and approximation
 	area = cv2.contourArea(cnt)
@@ -29,34 +30,34 @@ def contour(image = 'something', percent_arclength = 0.1):
 	#Convexity, k
 	k = cv2.isContourConvex(cnt)
 
-	print_dict = zip(["M", "cx", "cy", "area", "perimeter", "epsilon"] , [M, cx, cy, area, perimeter, epsilon])
+	print_dict = dict(zip(["M", "cx", "cy", "area", "perimeter", "epsilon", "k"] , [M, cx, cy, area, perimeter, epsilon, k]))
 	print(str(print_dict))
-	
+
 	#Bounding Rectangle
 	x,y,w,h = cv2.boundingRect(cnt)
-	img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+	img = cv2.rectangle(original_image,(x,y),(x+w,y+h),(0,255,0),2)
 
-	cv2.imwrite('outputs/BoundingRect.jpg', img)
+	cv2.imwrite('output/BoundingRect.jpg', img)
 
 	#Rotated Bounding Rectangle
 	rect = cv2.minAreaRect(cnt)
 	box = cv2.boxPoints(rect)
 	box = np.int0(box)
-	im = cv2.drawContours(im,[box],0,(0,0,255),2)
+	im = cv2.drawContours(original_image,[box],0,(0,0,255),2)
 
-	cv2.imwrite('outputs/BoundingRectRotated.jpg', im)
+	cv2.imwrite('output/BoundingRectRotated.jpg', im)
 
 	#Minimum Enclosing Circle
 	(x,y),radius = cv2.minEnclosingCircle(cnt)
 	center = (int(x),int(y))
 	radius = int(radius)
 	img = cv2.circle(img,center,radius,(0,255,0),2)
-	cv2.imwrite('outputs/MinCircle.jpg', img)
+	cv2.imwrite('output/MinCircle.jpg', img)
 
 	#Fitting Ellipse
 	ellipse = cv2.fitEllipse(cnt)
-	im = cv2.ellipse(im,ellipse,(0,255,0),2)
-	cv2.imwrite('outputs/FitEllipse.jpg', im)
+	im = cv2.ellipse(original_image,ellipse,(0,255,0),2)
+	cv2.imwrite('output/FitEllipse.jpg', im)
 	
 	#Fitting a line
 	rows,cols = img.shape[:2]
@@ -64,11 +65,11 @@ def contour(image = 'something', percent_arclength = 0.1):
 	lefty = int((-x*vy/vx) + y)
 	righty = int(((cols-x)*vy/vx)+y)
 	img = cv2.line(img,(cols-1,righty),(0,lefty),(0,255,0),2)
-	cv2.imwrite('outputs/FitEllipse.jpg', img)
+	cv2.imwrite('output/FitEllipse.jpg', img)
 
 
 def main():
-	contour()
+	contour(image = 'star-c.png')
 
 
 if __name__ == '__main__':
