7a4f90b74383aa6689d3b937268ab033e284efcf
Wilson Lin <wlin9608@gmail.com>
2019-10-11 14:59:27 -0700

    Added code from section 2.3

--- Lab3/serverTest.py
+++ Lab3/serverTest.py
@@ -1,8 +1,9 @@
 # Reminder: This is a comment. The first line imports a default library "socket" into Python.
-# You don't install this. The second line is initializtion to add TCP/Io
+# You don't install this. The second line is initialization to add TCP/IP protocol to the endpoint.
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Assigns a por tfor the server that listens to clients connecting to this port.
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
