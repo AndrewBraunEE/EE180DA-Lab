9e0389b7299a8632c29908aff4f8f001b34a360d
1337eeab <andrewtb97@hotmail.com>
2019-10-11 18:36:07 -0700

    MQTT

--- Lab3/mqtt_sub.py
+++ Lab3/mqtt_sub.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-MQTT_SERVER = "127.0.0.1" #mosquitto server ip address.
+MQTT_SERVER = "192.168.1.249" #mosquitto server ip address.
 MQTT_PATH = "ee180d/test" # name of a topic.
 # The callback for when the client receives a connect response from the server.
 def on_connect(client, userdata, flags, rc):
